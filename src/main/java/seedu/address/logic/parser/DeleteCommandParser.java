2a0acb3957bef6b8c69398cdb6b08c66547361ae
zuo yi <e1481832@u.nus.edu>
2025-10-30 20:15:46 +0800

    Combine logic of conditional delete and find

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -6,19 +6,13 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.FacultyContainsKeywordsPredicate;
-import seedu.address.model.person.ModuleContainsKeywordsPredicate;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.TagContainsKeywordsPredicate;
 
 
 /**
@@ -36,7 +30,7 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_TAG, PREFIX_FACULTY, PREFIX_MODULE);
 
         // Check if at least one prefix is present and preamble is empty
-        if (!isAnyPrefixPresent(argMultimap, PREFIX_NAME, PREFIX_TAG, PREFIX_FACULTY, PREFIX_MODULE)
+        if (!PredicateParserUtil.isAnyPrefixPresent(argMultimap, PREFIX_NAME, PREFIX_TAG, PREFIX_FACULTY, PREFIX_MODULE)
                 || !argMultimap.getPreamble().isEmpty()) {
 
             // Try parsing as index for single delete backward compatibility
@@ -53,51 +47,14 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
         // Verify no duplicate prefixes if parsing criteria
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_TAG, PREFIX_FACULTY, PREFIX_MODULE);
 
-        List<Predicate<Person>> predicates = new ArrayList<>();
-
-        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
-            String nameKeywords = argMultimap.getValue(PREFIX_NAME).get();
-            if (nameKeywords.isEmpty()) {
-                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
-            }
-            predicates.add(new NameContainsKeywordsPredicate(List.of(nameKeywords.split("\\s+"))));
-        }
-        if (argMultimap.getValue(PREFIX_TAG).isPresent()) {
-            String tagKeywords = argMultimap.getValue(PREFIX_TAG).get();
-            if (tagKeywords.isEmpty()) {
-                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
-            }
-            predicates.add(new TagContainsKeywordsPredicate(List.of(tagKeywords.split("\\s+"))));
-        }
-        if (argMultimap.getValue(PREFIX_FACULTY).isPresent()) {
-            String facultyKeywords = argMultimap.getValue(PREFIX_FACULTY).get();
-            if (facultyKeywords.isEmpty()) {
-                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
-            }
-            predicates.add(new FacultyContainsKeywordsPredicate(List.of(facultyKeywords.split("\\s+"))));
-        }
-        if (argMultimap.getValue(PREFIX_MODULE).isPresent()) {
-            String moduleKeywords = argMultimap.getValue(PREFIX_MODULE).get();
-            if (moduleKeywords.isEmpty()) {
-                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
-            }
-            predicates.add(new ModuleContainsKeywordsPredicate(List.of(moduleKeywords.split("\\s+"))));
-        }
+        String usageMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE);
+        List<Predicate<Person>> predicates = PredicateParserUtil.parsePredicates(argMultimap, usageMessage);
 
         if (predicates.isEmpty()) {
             // Should not happen if isAnyPrefixPresent check passed, but as a safeguard
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+            throw new ParseException(usageMessage);
         }
 
         return new DeleteCommand(predicates);
     }
-
-    /**
-     * Returns true if at least one of the prefixes contains a non-empty {@code Optional} value in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean isAnyPrefixPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).anyMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
-
 }
--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -3,21 +3,15 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_FACULTY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.FacultyContainsKeywordsPredicate;
-import seedu.address.model.person.ModuleContainsKeywordsPredicate;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.TagContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -33,7 +27,7 @@ public class FindCommandParser implements Parser<FindCommand> {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_TAG, PREFIX_FACULTY, PREFIX_MODULE);
 
-        if (!isAnyPrefixPresent(argMultimap, PREFIX_NAME, PREFIX_TAG, PREFIX_FACULTY, PREFIX_MODULE)
+        if (!PredicateParserUtil.isAnyPrefixPresent(argMultimap, PREFIX_NAME, PREFIX_TAG, PREFIX_FACULTY, PREFIX_MODULE)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
@@ -41,48 +35,12 @@ public class FindCommandParser implements Parser<FindCommand> {
         // Throws ParseException if prefixes are repeated e.g. n/Alice n/Bob
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_TAG, PREFIX_FACULTY, PREFIX_MODULE);
 
-        List<Predicate<Person>> predicates = new ArrayList<>();
-
-        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
-            String nameKeywords = argMultimap.getValue(PREFIX_NAME).get();
-            if (nameKeywords.isEmpty()) {
-                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
-            }
-            predicates.add(new NameContainsKeywordsPredicate(List.of(nameKeywords.split("\\s+"))));
-        }
-        if (argMultimap.getValue(PREFIX_TAG).isPresent()) {
-            String tagKeywords = argMultimap.getValue(PREFIX_TAG).get();
-            if (tagKeywords.isEmpty()) {
-                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
-            }
-            predicates.add(new TagContainsKeywordsPredicate(List.of(tagKeywords.split("\\s+"))));
-        }
-        if (argMultimap.getValue(PREFIX_FACULTY).isPresent()) {
-            String facultyKeywords = argMultimap.getValue(PREFIX_FACULTY).get();
-            if (facultyKeywords.isEmpty()) {
-                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
-            }
-            predicates.add(new FacultyContainsKeywordsPredicate(List.of(facultyKeywords.split("\\s+"))));
-        }
-        if (argMultimap.getValue(PREFIX_MODULE).isPresent()) {
-            String moduleKeywords = argMultimap.getValue(PREFIX_MODULE).get();
-            if (moduleKeywords.isEmpty()) {
-                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
-            }
-            predicates.add(new ModuleContainsKeywordsPredicate(List.of(moduleKeywords.split("\\s+"))));
-        }
+        String usageMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE);
+        List<Predicate<Person>> predicates = PredicateParserUtil.parsePredicates(argMultimap, usageMessage);
 
         // Combine all predicates with an 'AND' logic
         Predicate<Person> combinedPredicate = predicates.stream().reduce(Predicate::and).orElse(x -> true);
 
         return new FindCommand(combinedPredicate);
-    }
-
-    /**
-     * Returns true if at least one of the prefixes is present in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean isAnyPrefixPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).anyMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
